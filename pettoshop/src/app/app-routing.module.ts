7b3ce2e12670e54d933789bf42a62cc812f93266
maria bou jawde <maria.boujaoude@net.usj.edu.lb>
2022-11-22 17:15:25 +0100

    added item details

--- pettoshop/src/app/app-routing.module.ts
+++ pettoshop/src/app/app-routing.module.ts
@@ -5,13 +5,16 @@ import { LoginComponent } from './login/login.component';
 import { ItemsComponent } from './items/items.component';
 import { HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { ItemDetailsComponent } from './item-details/item-details.component';
 
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
   {path:'home', component:HomeComponent},
   {path:'login', component:LoginComponent},
-  {path:'items', component:ItemsComponent}
+  {path:'items', component:ItemsComponent},
+  {path:'item_details', component:ItemDetailsComponent},
+  {path:'item_details/:item', component:ItemDetailsComponent}
 ];
 
 @NgModule({
--- pettoshop/src/app/items/items.component.ts
+++ pettoshop/src/app/items/items.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Item } from '../models/item';
 import { ItemsService } from "../items.service";
 
@@ -11,19 +11,24 @@ import { ItemsService } from "../items.service";
 
 export class ItemsComponent implements OnInit {
 
-  items:any;
+  items: any;
 
-  constructor(public itemsService: ItemsService, private router:Router) {}
+  constructor(public itemsService: ItemsService, private router: Router) { }
 
   ngOnInit(): void {
     this.getItems();
   }
 
-  getItems(){
+  getItems() {
     this.itemsService.getItems().subscribe(
-      (items:Array<Item>)=>{
+      (items: Array<Item>) => {
         this.items = items;
       }
     )
   }
+
+  itemDetails(item: Item) {
+    if (item) this.router.navigate(['/item_details/:' + item]);
+  }
+
 }
